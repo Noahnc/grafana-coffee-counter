b696297bdbbfc655268115516f79e04cf1e679d9
Elia Seikritt <elia@seikritt.ch>
2023-12-27 00:57:37 +0100

    sensor reading moved to task

--- src/config.h
+++ src/config.h
@@ -24,6 +24,8 @@
 
 // Pin to which the vibration sensor is connected
 #define VIBRATION_SENSOR_PIN 36
+// Pin connected to a LED to indicated that vibration is detected
+#define VIBRATION_DETECTION_LED_VCC 25
 
 // Amount of loops to buffer a positive vibration signal.
 #define VIBRATION_BUFFER_LOOP_COUNT 50
