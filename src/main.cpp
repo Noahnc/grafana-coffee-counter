6b903cc96a037d19ab071b93028f00accf42f169
Noah Canadea <noahnc@MacBook-Pro-von-Noah.local>
2024-01-14 11:37:22 +0100

    feat(main): Update main * Remove heltec board. * Add additional counters. * Refactor metric ingestion.

--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,14 @@
 #include <config.h>
 #include <tuple>
 
-#ifdef heltec_wifi_kit32
-#include "heltec.h"
-#endif
-
 #include <vibration_detect.h>
 #include <transport.h>
 
 Transport transport(WIFI_STATUS_LED_VCC, WIFI_SSID, WIFI_PASSWORD);
 
-int64_t coffee_count_total = 0;
+int64_t small_coffee_count = 0;
+int64_t medium_coffee_count = 0;
+int64_t large_coffee_count = 0;
 int64_t current_time = 0;
 int64_t last_metric_ingestion = 0;
 int64_t last_remote_write = 0;
@@ -24,22 +22,23 @@ hw_timer_t *timer0 = NULL;
 // background task for vibration detection
 TaskHandle_t vibration_detection_task;
 // counter for the detected vibrations
-SemaphoreHandle_t vibration_counter_sem;
+SemaphoreHandle_t coffee_counters_sem;
 
 // Function prototypes
 bool detectMotion();
-void updateDisplay();
-String performRemoteWrite();
+bool performRemoteWrite();
 bool handleMotionBuffer();
 void handleSampleIngestion();
 void handleMetricsSend();
-void ingestMetricSample(TimeSeries &ts, int64_t timestamp, int64_t value);
+void ingestMetricSample(TimeSeries &ts, int64_t timestamp, int64_t value, String name);
 
 WriteRequest req(2, 1024);
 
 const char *labels = "{job=\"cmi_coffee_counter\",location=\"schwerzenbach_4OG\"}";
 // TimeSeries that can hold 5 samples each. Make sure to set sample_ingestation rate and remote_write_interval accordingly
-TimeSeries coffees_consumed(5, "coffees_consumed_counter", labels);
+TimeSeries coffees_consumed_small(5, "coffees_consumed_small_count", labels);
+TimeSeries coffees_consumed_medium(5, "coffees_consumed_medium_count", labels);
+TimeSeries coffees_consumed_large(5, "coffees_consumed_large_count", labels);
 TimeSeries system_memory_free_bytes(5, "system_memory_free_bytes", labels);
 TimeSeries system_memory_total_bytes(5, "system_memory_total_bytes", labels);
 TimeSeries system_network_wifi_rssi(5, "system_network_wifi_rssi", labels);
@@ -60,22 +59,18 @@ void setup()
 
   // setup background task for vibration detection
   timer0 = timerBegin(0, 80, true);
-  vibration_counter_sem = xSemaphoreCreateBinary();
-  xSemaphoreGive(vibration_counter_sem);
+  coffee_counters_sem = xSemaphoreCreateBinary();
+  xSemaphoreGive(coffee_counters_sem);
   vibration_detection_parameters parameters;
-  parameters.detection_threshold_ms = MOTION_DETECTION_DURATION_SECONDS * 1000;
+  parameters.detection_threshold_ms_small_coffee = MOTION_DETECTION_DURATION_SECONDS_SMALL_COFFEE * 1000;
+  parameters.detection_threshold_ms_medium_coffee = MOTION_DETECTION_DURATION_SECONDS_MEDIUM_COFFEE * 1000;
+  parameters.detection_threshold_ms_large_coffee = MOTION_DETECTION_DURATION_SECONDS_LARGE_COFFEE * 1000;
   parameters.timer = timer0;
-  parameters.vibration_counter_sem = &vibration_counter_sem;
-  parameters.vibration_counter = &coffee_count_total;
+  parameters.vibration_counter_sem = &coffee_counters_sem;
+  parameters.vibration_counter_small_coffee = &small_coffee_count;
+  parameters.vibration_counter_medium_coffee = &medium_coffee_count;
+  parameters.vibration_counter_large_coffee = &large_coffee_count;
   create_vibration_dection_task(&vibration_detection_task, &parameters);
-
-#ifdef heltec_wifi_kit32
-  Heltec.begin(true, false, true, true, 915E6);
-  Heltec.display->setContrast(255);
-  Heltec.display->clear();
-  Heltec.display->drawString(0, 0, "Booting ...");
-  Heltec.display->display();
-#endif
 
   // setup transportation
   transport.setEndpoint(GC_PORT, GC_URL, (char *)GC_PATH);
@@ -86,7 +81,9 @@ void setup()
   }
   transport.beginAsync();
 
-  req.addTimeSeries(coffees_consumed);
+  req.addTimeSeries(coffees_consumed_small);
+  req.addTimeSeries(coffees_consumed_medium);
+  req.addTimeSeries(coffees_consumed_large);
   req.addTimeSeries(system_memory_free_bytes);
   req.addTimeSeries(system_memory_total_bytes);
   req.addTimeSeries(system_network_wifi_rssi);
@@ -97,11 +94,6 @@ void setup()
   current_time = 0;
   last_metric_ingestion = 0;
   last_remote_write = 0;
-
-#ifdef heltec_wifi_kit32
-  Heltec.display->clear();
-  Heltec.display->drawString(0, 0, "Startup done");
-#endif
 };
 
 void loop()
@@ -110,9 +102,6 @@ void loop()
 
   handleSampleIngestion();
   handleMetricsSend();
-
-  // Update the display
-  updateDisplay();
 
   vTaskDelay(50 / portTICK_PERIOD_MS);
 }
@@ -123,9 +112,9 @@ void handleMetricsSend()
   {
     if (DEBUG)
       Serial.println("Performing remote write");
-    String res = performRemoteWrite();
+    bool success = performRemoteWrite();
     if (DEBUG)
-      Serial.println("Remote write result: " + res);
+      Serial.println("Remote Write successfull: " + String(success));
     last_remote_write = transport.getTimeMillis();
   }
 }
@@ -136,62 +125,49 @@ void handleSampleIngestion()
   {
     return;
   }
-  if (xSemaphoreTake(vibration_counter_sem, (TickType_t)10) == pdTRUE)
+  if (xSemaphoreTake(coffee_counters_sem, (TickType_t)10) == pdTRUE)
   {
-    Serial.println("Total coffee count: " + String(coffee_count_total));
-    ingestMetricSample(coffees_consumed, current_time, coffee_count_total);
+    ingestMetricSample(coffees_consumed_small, current_time, small_coffee_count, "small_coffee_count");
+    ingestMetricSample(coffees_consumed_medium, current_time, medium_coffee_count, "medium_coffee_count");
+    ingestMetricSample(coffees_consumed_large, current_time, large_coffee_count, "large_coffee_count");
 
-    xSemaphoreGive(vibration_counter_sem);
+    xSemaphoreGive(coffee_counters_sem);
   }
-  ingestMetricSample(system_memory_free_bytes, current_time, ESP.getFreeHeap());
-  ingestMetricSample(system_memory_total_bytes, current_time, ESP.getHeapSize());
-  ingestMetricSample(system_network_wifi_rssi, current_time, WiFi.RSSI());
-  ingestMetricSample(system_largest_heap_block_size_bytes, current_time, ESP.getMaxAllocHeap());
+  ingestMetricSample(system_memory_free_bytes, current_time, ESP.getFreeHeap(), "free_heap_bytes");
+  ingestMetricSample(system_memory_total_bytes, current_time, ESP.getHeapSize(), "total_heap_bytes");
+  ingestMetricSample(system_network_wifi_rssi, current_time, WiFi.RSSI(), "wifi_rssi");
+  ingestMetricSample(system_largest_heap_block_size_bytes, current_time, ESP.getMaxAllocHeap(), "largest_heap_block_bytes");
   last_metric_ingestion = transport.getTimeMillis();
 }
 
-void ingestMetricSample(TimeSeries &ts, int64_t timestamp, int64_t value)
+void ingestMetricSample(TimeSeries &ts, int64_t timestamp, int64_t value, String name)
 {
   if (ts.addSample(timestamp, value))
   {
     if (DEBUG)
-    {
-      Serial.println("Ingesting metrics: Sample added");
-    }
+      Serial.println("Ingesting metrics for " + name + ": " + String(value) + " at " + String(timestamp));
   }
   else
   {
     if (DEBUG)
-    {
       Serial.println("Ingesting metrics: Failed to add sample" + String(ts.errmsg));
-    }
   }
 }
 
-String performRemoteWrite()
+bool performRemoteWrite()
 {
   Serial.println("Performing remote write");
   PromClient::SendResult res = transport.send(req);
   if (!res == PromClient::SendResult::SUCCESS)
   {
-    return "error";
+    return false;
   }
-  coffees_consumed.resetSamples();
+  coffees_consumed_small.resetSamples();
+  coffees_consumed_medium.resetSamples();
+  coffees_consumed_large.resetSamples();
   system_memory_free_bytes.resetSamples();
   system_memory_total_bytes.resetSamples();
   system_network_wifi_rssi.resetSamples();
   system_largest_heap_block_size_bytes.resetSamples();
-  return "success";
-}
-
-void updateDisplay()
-{
-#ifdef heltec_wifi_kit32
-  if (xSemaphoreTake(vibration_counter_sem, (TickType_t)10) == pdTRUE)
-  {
-    Heltec.display->drawString(0, 0, "Coffee Total Count: " + String(coffee_count_total));
-    Heltec.display->display();
-    xSemaphoreGive(vibration_counter_sem);
-  }
-#endif
+  return true;
 }
