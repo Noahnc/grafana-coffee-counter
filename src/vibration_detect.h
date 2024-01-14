4a190634dc98eb4cd916a0fd269eac4157991715
Noah Canadea <noahnc@MacBook-Pro-von-Noah.local>
2024-01-14 11:35:32 +0100

    feat(vibration_detection): Add additional counters to differentiate between three sizes of coffee

--- src/vibration_detect.h
+++ src/vibration_detect.h
@@ -4,15 +4,20 @@
 #include "config.h"
 #include <Arduino.h>
 
-typedef struct {
+typedef struct
+{
     /// @brief Timer to use for time measuring.
     hw_timer_t *timer;
-    /// @brief Minimum time a vibration must persist to be detected.
-    int32_t detection_threshold_ms;
+    /// @brief Minimum vibration detection time in ms for the differnet coffee types.
+    int32_t detection_threshold_ms_small_coffee;
+    int32_t detection_threshold_ms_medium_coffee;
+    int32_t detection_threshold_ms_large_coffee;
     /// @brief Semaphore that must be taken to update the counter.
     SemaphoreHandle_t *vibration_counter_sem;
-    /// @brief Counter to increase when a vibration is detected.
-    int64_t *vibration_counter;
+    /// @brief Counter to increase for the different coffee types.
+    int64_t *vibration_counter_small_coffee;
+    int64_t *vibration_counter_medium_coffee;
+    int64_t *vibration_counter_large_coffee;
 } vibration_detection_parameters;
 
 BaseType_t create_vibration_dection_task(TaskHandle_t *task_handle, vibration_detection_parameters *parameter);
