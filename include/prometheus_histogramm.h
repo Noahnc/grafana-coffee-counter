34c4df590c2c2d891b56bd5c14b9bbf4b3673d74
Noah Canadea <noahnc@MacBook-Pro-von-Noah.local>
2024-01-15 20:22:19 +0100

    feat(histogramm): Add histogramm helper class

---
+++ include/prometheus_histogramm.h
@@ -0,0 +1,32 @@
+#ifndef Prometheus_Histogramm_INCLUDED
+#define Prometheus_Histogramm_INCLUDED
+
+#include "config.h"
+#include <Arduino.h>
+#include <PrometheusArduino.h>
+
+class Prometheus_Histogramm
+{
+private:
+    TimeSeries **time_series_buckets;
+    TimeSeries time_series_count;
+    TimeSeries time_series_sum;
+    int16_t bucket_start;
+    int16_t bucket_increment;
+    int16_t bucket_count;
+    int16_t series_size;
+    int16_t *bucket_values;
+    int16_t *bucket_counters;
+    int64_t sum;
+    int16_t count;
+    char *name;
+    char *labels;
+
+public:
+    Prometheus_Histogramm(char *name, char *labels, int16_t series_size, int16_t bucket_start, int16_t bucket_increment, int16_t bucket_count);
+    void init();
+    void AddValue(int16_t value);
+    void Ingest(int64_t timestamp);
+};
+
+#endif
