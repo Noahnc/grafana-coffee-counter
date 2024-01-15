feaba95bfb8703e73d257409b72136dfcefffd19
Noah Canadea <noahnc@MacBook-Pro-von-Noah.local>
2024-01-15 20:42:47 +0100

    fix(histogramm): Add inf series.

--- src/prometheus_histogramm.cpp
+++ src/prometheus_histogramm.cpp
@@ -9,7 +9,7 @@ Prometheus_Histogramm::Prometheus_Histogramm(char *name, char *labels, int16_t s
     this->series_size = series_size;
     this->bucket_start = bucket_start;
     this->bucket_increment = bucket_increment;
-    this->bucket_count = bucket_count;
+    this->bucket_count = bucket_count + 1;
     this->bucket_values = new int16_t[bucket_count];
     this->bucket_counters = new int16_t[bucket_count];
     time_series_buckets = new TimeSeries *[bucket_count];
@@ -33,8 +33,18 @@ void Prometheus_Histogramm::init()
         if (closing_brace_pos != std::string::npos)
         {
             // Insert the new label before the closing brace
-            std::string new_label = ",le=" + std::to_string(this->bucket_values[i]);
-            bucket_labels.insert(closing_brace_pos, new_label);
+            if (i == bucket_count - 1)
+            {
+                // The last bucket is labeled with "+Inf"
+                std::string new_label = ",le=\"+Inf\"";
+                bucket_labels.insert(closing_brace_pos, new_label);
+            }
+            else
+            {
+                // All other buckets are labeled with the upper bound of the bucket (exclusive
+                std::string new_label = ",le=\"" + std::to_string(this->bucket_values[i]) + "\"";
+                bucket_labels.insert(closing_brace_pos, new_label);
+            }
         }
 
         // Initialize the TimeSeries object for the current bucket
@@ -52,13 +62,21 @@ void Prometheus_Histogramm::init()
 
 void Prometheus_Histogramm::AddValue(int16_t value)
 {
+    bool found = false;
+    // Increment all bucket counters for which the value is smaller than the bucket value
     for (int i = 0; i < bucket_count; i++)
     {
         if (value <= bucket_values[i])
         {
             bucket_counters[i] += 1;
+            found = true;
             break;
         }
+    }
+    if (!found)
+    {
+        // Increment the counter for the last bucket (which is labeled with "+Inf")
+        bucket_counters[bucket_count - 1] += 1;
     }
     sum += value;
     count += 1;
