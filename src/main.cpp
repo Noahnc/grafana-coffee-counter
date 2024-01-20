2b442e91f40bb410b6441a5492d0fabdce2707b6
Noah Canadea <noahnc@MacBook-Pro-von-Noah.local>
2024-01-20 13:19:14 +0100

    feat(main): * Add comments. * Set all time variables to startup time in setup. * Switch Histogramm input form seconds to ms.

--- src/main.cpp
+++ src/main.cpp
@@ -12,9 +12,11 @@
 
 Transport transport(WIFI_STATUS_LED_VCC, WIFI_SSID, WIFI_PASSWORD);
 
-int64_t current_time = 0;
-int64_t last_metric_ingestion = 0;
-int64_t last_remote_write = 0;
+// Setup time variables
+int64_t start_time;
+int64_t current_time;
+int64_t last_metric_ingestion;
+int64_t last_remote_write;
 
 // timer 0 of esp32 for vibration detection
 hw_timer_t *timer0 = NULL;
@@ -31,16 +33,17 @@ void handleSampleIngestion();
 void handleMetricsSend();
 void ingestMetricSample(TimeSeries &ts, int64_t timestamp, int64_t value, String name);
 
-WriteRequest req(2, 1024);
+// The write request that will be used to send the metrics to Prometheus. For every Histogramm you need to add 2 + number of buckets timeseries
+WriteRequest req(16, 8192);
 
 char *labels = "{job=\"cmi_coffee_counter\",location=\"schwerzenbach_4OG\"}";
 
 // TimeSeries that can hold 5 samples each. Make sure to set sample_ingestation rate and remote_write_interval accordingly
-Prometheus_Histogramm coffees_consumed("coffees_consumed", labels, 5, 10, 4, 10);
-TimeSeries system_memory_free_bytes(5, "system_memory_free_bytes", labels);
-TimeSeries system_memory_total_bytes(5, "system_memory_total_bytes", labels);
-TimeSeries system_network_wifi_rssi(5, "system_network_wifi_rssi", labels);
-TimeSeries system_largest_heap_block_size_bytes(5, "system_largest_heap_block_size_bytes", labels);
+Prometheus_Histogramm coffees_consumed("coffees_consumed", labels, 10, 10000, 4000, 10);
+TimeSeries system_memory_free_bytes(10, "system_memory_free_bytes", labels);
+TimeSeries system_memory_total_bytes(10, "system_memory_total_bytes", labels);
+TimeSeries system_network_wifi_rssi(10, "system_network_wifi_rssi", labels);
+TimeSeries system_largest_heap_block_size_bytes(10, "system_largest_heap_block_size_bytes", labels);
 
 void setup()
 {
@@ -48,6 +51,13 @@ void setup()
   pinMode(VIBRATION_DETECTION_LED_VCC, OUTPUT);
   pinMode(WIFI_STATUS_LED_VCC, OUTPUT);
 
+  // Set all time variables to the current startup time
+  start_time = transport.getTimeMillis();
+  current_time = start_time;
+  last_metric_ingestion = start_time;
+  last_remote_write = start_time;
+
+  // Setup serial
   Serial.begin(SERIAL_BAUD);
   while (!Serial)
     ;
@@ -55,11 +65,15 @@ void setup()
   Serial.println("Starting up coffe counter ...");
   Serial.println("WiFi SSID: " + String(WIFI_SSID));
 
-  // setup background task for vibration detection
+  // setup background task for vibration detection with semaphore
   timer0 = timerBegin(0, 80, true);
   coffee_counters_sem = xSemaphoreCreateBinary();
   xSemaphoreGive(coffee_counters_sem);
+
+  // init coffees_consumed histogramm
   coffees_consumed.init(&req);
+
+  // setup vibration detection task with parameters
   vibration_detection_parameters parameters;
   parameters.vibration_detection_threshold_ms = MOTION_DETECTION_DURATION_THREASHOLD_SECONDS * 1000;
   parameters.timer = timer0;
@@ -67,7 +81,7 @@ void setup()
   parameters.coffees_consumed = &coffees_consumed;
   create_vibration_dection_task(&vibration_detection_task, &parameters);
 
-  // setup transportation
+  // setup transportation to Grafana Cloud
   transport.setEndpoint(GC_PORT, GC_URL, (char *)GC_PATH);
   transport.setCredentials(GC_USER, GC_PASS);
   if (DEBUG)
@@ -76,16 +90,14 @@ void setup()
   }
   transport.beginAsync();
 
+  // Add system metrics to the write request
   req.addTimeSeries(system_memory_free_bytes);
   req.addTimeSeries(system_memory_total_bytes);
   req.addTimeSeries(system_network_wifi_rssi);
   req.addTimeSeries(system_largest_heap_block_size_bytes);
+
   if (DEBUG)
     Serial.println("Startup done");
-
-  current_time = 0;
-  last_metric_ingestion = 0;
-  last_remote_write = 0;
 };
 
 void loop()
@@ -93,7 +105,7 @@ void loop()
   current_time = transport.getTimeMillis();
 
   handleSampleIngestion();
-  handleMetricsSend();
+  // handleMetricsSend();
 
   vTaskDelay(50 / portTICK_PERIOD_MS);
 }
@@ -117,6 +129,8 @@ void handleSampleIngestion()
   {
     return;
   }
+  if (DEBUG)
+    Serial.println("Ingesting metrics");
   if (xSemaphoreTake(coffee_counters_sem, (TickType_t)10) == pdTRUE)
   {
     coffees_consumed.Ingest(current_time);
