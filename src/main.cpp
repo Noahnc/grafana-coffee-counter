5918fd5e11ae36fd2e924fab3965cde004eaa423
Elia Seikritt <elia@seikritt.ch>
2024-01-21 17:29:41 +0100

    Set site/floor label via environment

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,9 @@
 #include <config.h>
 #include <tuple>
 #include <stdio.h>
-#include <cstring>
+#include <string>
+#include <vector>
+#include <sstream>
 #include <vibration.h>
 #include <transport.h>
 #include <prometheus_histogram.h>
@@ -18,6 +20,8 @@ bool performRemoteWrite();
 void handleSampleIngestion();
 void handleMetricsSend();
 void ingestMetricSample(TimeSeries &ts, int64_t timestamp, int64_t value, String name);
+std::vector<std::string> setupLabels();
+std::string joinLabels(const std::vector<std::string> &strings);
 
 // Setup time variables
 int64_t start_time_unix_ms = 0;
@@ -32,20 +36,19 @@ int remote_write_failures = 0;
 // The write request that will be used to send the metrics to Prometheus. For every Histogramm you need to add 3 + number of buckets timeseries
 WriteRequest req(18, 4096);
 
-char *labels = "{job=\"cmi_coffee_counter\",location=\"schwerzenbach_4OG\"}";
+// TimeSeries and labels
+const char *labels;
+Prometheus_Histogram *coffees_consumed = nullptr;
+TimeSeries *system_memory_free_bytes = nullptr;
+TimeSeries *system_memory_total_bytes = nullptr;
+TimeSeries *system_network_wifi_rssi = nullptr;
+TimeSeries *system_largest_heap_block_size_bytes = nullptr;
+TimeSeries *system_run_time_ms = nullptr;
+TimeSeries *system_remote_write_failures_count = nullptr;
 
-// TimeSeries that can hold 5 samples each. Make sure to set sample_ingestation rate and remote_write_interval accordingly
-Prometheus_Histogram coffees_consumed("coffees_consumed", labels, 5, 10000, 4000, 10);
-TimeSeries system_memory_free_bytes(5, "system_memory_free_bytes", labels);
-TimeSeries system_memory_total_bytes(5, "system_memory_total_bytes", labels);
-TimeSeries system_network_wifi_rssi(5, "system_network_wifi_rssi", labels);
-TimeSeries system_largest_heap_block_size_bytes(5, "system_largest_heap_block_size_bytes", labels);
-TimeSeries system_run_time_ms(5, "system_run_time_ms", labels);
-TimeSeries system_remote_write_failures_count(5, "system_remote_write_failures_count", labels);
-
+// helper services
 hw_timer_t *timer0 = timerBegin(0, 80, true); // timer 0 of esp32 for vibration detection
-Vibration vibration(timer0, MOTION_DETECTION_DURATION_THREASHOLD_SECONDS * 1000, &coffees_consumed);
-
+Vibration vibration(timer0, MOTION_DETECTION_DURATION_THREASHOLD_SECONDS * 1000, coffees_consumed);
 Transport transport(WIFI_STATUS_LED_VCC, WIFI_SSID, WIFI_PASSWORD);
 
 void setup()
@@ -62,11 +65,22 @@ void setup()
   Serial.println("Starting up coffe counter ...");
   Serial.println("WiFi SSID: " + String(WIFI_SSID));
 
+  // TimeSeries that can hold 5 samples each. Make sure to set sample_ingestation rate and remote_write_interval accordingly
+  std::vector<std::string> labelVector = setupLabels();
+  labels = joinLabels(labelVector).c_str();
+  coffees_consumed = new Prometheus_Histogram("coffees_consumed", labels, 5, 10000, 4000, 10);
+  system_memory_free_bytes = new TimeSeries(5, "system_memory_free_bytes", labels);
+  system_memory_total_bytes = new TimeSeries(5, "system_memory_total_bytes", labels);
+  system_network_wifi_rssi = new TimeSeries(5, "system_network_wifi_rssi", labels);
+  system_largest_heap_block_size_bytes = new TimeSeries(5, "system_largest_heap_block_size_bytes", labels);
+  system_run_time_ms = new TimeSeries(5, "system_run_time_ms", labels);
+  system_remote_write_failures_count = new TimeSeries(5, "system_remote_write_failures_count", labels);
+
   // setup background task for vibration detection
   vibration.beginAsync();
 
   // init coffees_consumed histogram
-  coffees_consumed.init(req);
+  coffees_consumed->init(req);
 
   // setup transportation to Grafana Cloud
   transport.setEndpoint(GC_PORT, GC_URL, (char *)GC_PATH);
@@ -79,10 +93,12 @@ void setup()
   transport.beginAsync();
 
   // Add system metrics to the write request
-  req.addTimeSeries(system_memory_free_bytes);
-  req.addTimeSeries(system_memory_total_bytes);
-  req.addTimeSeries(system_network_wifi_rssi);
-  req.addTimeSeries(system_largest_heap_block_size_bytes);
+  req.addTimeSeries(*system_memory_free_bytes);
+  req.addTimeSeries(*system_memory_total_bytes);
+  req.addTimeSeries(*system_network_wifi_rssi);
+  req.addTimeSeries(*system_largest_heap_block_size_bytes);
+  req.addTimeSeries(*system_run_time_ms);
+  req.addTimeSeries(*system_remote_write_failures_count);
 
   // Set all time variables to the current startup time
   start_time_unix_ms = transport.getTimeMillis();
@@ -102,6 +118,35 @@ void loop()
   handleMetricsSend();
 
   vTaskDelay(4000 / portTICK_PERIOD_MS);
+}
+
+std::vector<std::string> setupLabels()
+{
+  char instanceLabel[25];
+  sprintf(instanceLabel, "instance=\"%012llX\"", ESP.getEfuseMac());
+
+  std::vector<std::string> labels;
+  labels.push_back("job=\"cmi_coffee_counter\"");
+  labels.push_back(std::string(instanceLabel));
+  labels.push_back("site=\"" + std::string(METRICS_LABEL_SITE) + "\"");
+  labels.push_back("floor=\"" + std::string(METRICS_LABEL_FLOOR) + "\"");
+  return labels;
+}
+
+std::string joinLabels(const std::vector<std::string> &strings)
+{
+  std::ostringstream joined;
+  joined << "{";
+  for (size_t i = 0; i < strings.size(); ++i)
+  {
+    joined << strings[i];
+    if (i < strings.size() - 1)
+    {
+      joined << ",";
+    }
+  }
+  joined << "}";
+  return joined.str();
 }
 
 void handleMetricsSend()
@@ -142,13 +187,13 @@ void handleSampleIngestion()
   if (DEBUG)
     Serial.println("Ingesting metrics");
 
-  coffees_consumed.Ingest(current_cicle_start_time_unix_ms);
-  ingestMetricSample(system_memory_free_bytes, current_cicle_start_time_unix_ms, ESP.getFreeHeap(), "free_heap_bytes");
-  ingestMetricSample(system_memory_total_bytes, current_cicle_start_time_unix_ms, ESP.getHeapSize(), "total_heap_bytes");
-  ingestMetricSample(system_network_wifi_rssi, current_cicle_start_time_unix_ms, WiFi.RSSI(), "wifi_rssi");
-  ingestMetricSample(system_largest_heap_block_size_bytes, current_cicle_start_time_unix_ms, ESP.getMaxAllocHeap(), "largest_heap_block_bytes");
-  ingestMetricSample(system_run_time_ms, current_cicle_start_time_unix_ms, run_time_ms, "run_time_ms");
-  ingestMetricSample(system_remote_write_failures_count, current_cicle_start_time_unix_ms, remote_write_failures, "remote_write_failures_count");
+  coffees_consumed->Ingest(current_cicle_start_time_unix_ms);
+  ingestMetricSample(*system_memory_free_bytes, current_cicle_start_time_unix_ms, ESP.getFreeHeap(), "free_heap_bytes");
+  ingestMetricSample(*system_memory_total_bytes, current_cicle_start_time_unix_ms, ESP.getHeapSize(), "total_heap_bytes");
+  ingestMetricSample(*system_network_wifi_rssi, current_cicle_start_time_unix_ms, WiFi.RSSI(), "wifi_rssi");
+  ingestMetricSample(*system_largest_heap_block_size_bytes, current_cicle_start_time_unix_ms, ESP.getMaxAllocHeap(), "largest_heap_block_bytes");
+  ingestMetricSample(*system_run_time_ms, current_cicle_start_time_unix_ms, run_time_ms, "run_time_ms");
+  ingestMetricSample(*system_remote_write_failures_count, current_cicle_start_time_unix_ms, remote_write_failures, "remote_write_failures_count");
   last_metric_ingestion_unix_ms = transport.getTimeMillis();
 }
 
@@ -173,12 +218,12 @@ bool performRemoteWrite()
   {
     return false;
   }
-  coffees_consumed.resetSamples();
-  system_memory_free_bytes.resetSamples();
-  system_memory_total_bytes.resetSamples();
-  system_network_wifi_rssi.resetSamples();
-  system_largest_heap_block_size_bytes.resetSamples();
-  system_run_time_ms.resetSamples();
-  system_remote_write_failures_count.resetSamples();
+  coffees_consumed->resetSamples();
+  system_memory_free_bytes->resetSamples();
+  system_memory_total_bytes->resetSamples();
+  system_network_wifi_rssi->resetSamples();
+  system_largest_heap_block_size_bytes->resetSamples();
+  system_run_time_ms->resetSamples();
+  system_remote_write_failures_count->resetSamples();
   return true;
 }
